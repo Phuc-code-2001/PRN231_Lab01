cd3b11e0fefab0ae6fdd5a0c20c86564aa41b5ca
phuchtce150394 <phuchtce150394@fpt.edu.vn>
2022-09-15 21:48:05 +0700

    activity_01: Create 5 project in a solution

---
+++ Lab01_ASP.NETCoreWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab01_ASP.NETCoreWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
