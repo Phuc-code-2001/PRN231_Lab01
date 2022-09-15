e79820bccb9d9ed084f49736e569c2fdcfcf266e
phuchtce150394 <phuchtce150394@fpt.edu.vn>
2022-09-15 22:25:56 +0700

    Activity02: Create database with 2 table. Category and Product

---
+++ BusinessObjects/Domain/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Domain
+{
+    public class Category
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string Name { get; set; }
+
+        public virtual IList<Product> Products { get; set; }
+    }
+}
