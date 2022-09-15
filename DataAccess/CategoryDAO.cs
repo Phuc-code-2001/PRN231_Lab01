6dac637ad947774b0c7164e27e6fc98d7843a427
phuchtce150394 <phuchtce150394@fpt.edu.vn>
2022-09-15 22:42:30 +0700

    Activity03: Create CaterogyDAO, ProductDAO with CRUD function

---
+++ DataAccess/CategoryDAO.cs
@@ -0,0 +1,28 @@
+﻿using BusinessObjects.DataContext;
+using BusinessObjects.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class CategoryDAO
+    {
+        public static List<Category> GetCategories()
+        {
+            try
+            {
+                using(var context = new ApplicationDBContext())
+                {
+                    return context.Categories.ToList();
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Something went wrong: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ DataAccess/ProductDAO.cs
@@ -0,0 +1,91 @@
+﻿using BusinessObjects.DataContext;
+using BusinessObjects.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ProductDAO
+    {
+        public static List<Product> GetProducts()
+        {
+            try
+            {
+                using(var context = new ApplicationDBContext())
+                {
+                    return context.Products.ToList();
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Something went wrong: {ex.Message}");
+            }
+        }
+
+        public static Product FindProductById(int Id)
+        {
+            try
+            {
+                using (var context = new ApplicationDBContext())
+                {
+                    return context.Products.FirstOrDefault(p => p.Id == Id);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong: {ex.Message}");
+            }
+        }
+
+        public static void SaveProduct(Product p)
+        {
+            try
+            {
+                using (var context = new ApplicationDBContext())
+                {
+                    context.Products.Add(p);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong: {ex.Message}");
+            }
+        }
+        public static void UpdateProduct(Product p)
+        {
+            try
+            {
+                using (var context = new ApplicationDBContext())
+                {
+                    context.Products.Update(p);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong: {ex.Message}");
+            }
+        }
+        public static void DeleteProduct(Product p)
+        {
+            try
+            {
+                using (var context = new ApplicationDBContext())
+                {
+                    var p1 = context.Products.FirstOrDefault(pro => pro.Id == p.Id);
+                    context.Products.Remove(p1);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong: {ex.Message}");
+            }
+        }
+
+    }
+}
